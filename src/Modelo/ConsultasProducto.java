148f214e8ba95af56181dea1c39754e608af26a8
criss <crisswebapp@gmail.com>
2019-09-22 21:33:39 -0600

    > creacion de clase producto y sus metodos

--- src/Modelo/ConsultasProducto.java
+++ src/Modelo/ConsultasProducto.java
@@ -5,10 +5,152 @@
  */
 package Modelo;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  *
  * @author criss
  */
 public class ConsultasProducto extends Pool {
+    
+    public boolean registrar(Producto pro){
+        
+        PreparedStatement ps = null;
+        Connection cn = (Connection)getConnection();
+        String sql = "INSERT INTO producto(codigo, nombre, descripcion,"
+                + "categoria, marca) values(?,?,?,?,?)";
+        
+        try{
+             ps = (PreparedStatement) cn.prepareStatement(sql);
+             ps.setString(1, pro.getCodigo());
+             ps.setString(2, pro.getNombre());
+             ps.setString(3, pro.getDescripcion());
+             ps.setString(4, pro.getCategoria());
+             ps.setString(5, pro.getMarca());
+             ps.execute();
+             return true;
+             
+        }catch(SQLException e){
+            System.err.print(e);
+            return false;
+            
+        }finally{
+            if(cn!=null){
+                try{
+                    cn.close();
+                }catch(SQLException e){
+                    System.err.print(e);
+                }
+            }
+        }
+      
+    }
+    
+    
+    public boolean modificar(Producto pro){
+        PreparedStatement ps = null;
+        Connection cn = (Connection)getConnection();
+        String sql = "UPDATE producto SET codigo=?, nombre=?, descripcion=?"
+                + "categoria=?, marca=?"
+                + "WHERE idProducto=?";
+        try{
+            ps = (PreparedStatement)cn.prepareStatement(sql);
+            ps.setString(1, pro.getCodigo());
+            ps.setString(2, pro.getNombre());
+            ps.setString(3, pro.getDescripcion());
+            ps.setString(4, pro.getCategoria());
+            ps.setString(5, pro.getMarca());
+            ps.setInt(6, pro.getIdProducto());
+            ps.execute();
+            return true;            
+            
+        }catch(SQLException e){
+            System.err.print(e);
+            return false;
+        }finally{
+            if(cn!=null){
+                try{
+                    cn.close();
+                }catch (SQLException e){
+                    System.err.print(e);
+                }
+            }
+        }
+        
+        
+        
+    }
+    public boolean eliminar(Producto pro){
+        PreparedStatement ps = null;
+        Connection cn = (Connection)getConnection();
+        String sql = "DELETE FROM producto WHERE idProducto=?";
+        
+        try{
+            ps = (PreparedStatement)cn.prepareStatement(sql);
+            ps.setInt(1, pro.getIdProducto());
+            ps.execute();
+            return true;            
+            
+        }catch(SQLException e){
+            System.err.print(e);
+            return false;
+        }finally{
+            if(cn!=null){
+                try{
+                    cn.close();
+                }catch (SQLException e){
+                    System.err.print(e);
+                }
+            }
+        }
+        
+        
+    }
+    
+    
+    
+    public boolean consultar(Producto pro){
+        PreparedStatement ps = null;
+        Connection cn = (Connection)getConnection();
+        ResultSet rs = null;
+        String sql ="SELECT * FROM producto WHERE idProducto=?";
+        try{
+            ps = (PreparedStatement)cn.prepareStatement(sql);
+            ps.setInt(1, pro.getIdProducto());
+            rs =ps.executeQuery();
+            if(rs.next()){
+                pro.setIdProducto(rs.getInt("idProducto"));
+                pro.setCodigo(rs.getString("codigo"));
+                pro.setNombre(rs.getString("nombre"));
+                pro.setDescripcion(rs.getString("descripcion"));
+                pro.setCategoria(rs.getString("categoria"));
+                pro.setMarca(rs.getString("marca"));
+
+             
+            } 
+            return true;
+            
+        }catch (SQLException e){
+            System.err.print(e);
+            return false;
+        }finally{
+            if(cn!=null){
+                try{
+                    cn.close();
+                }catch(SQLException e){
+                    System.err.print(e);
+                }
+                
+            }
+        }
+        
+    }
+    
+    
+    
+    
     
 }
